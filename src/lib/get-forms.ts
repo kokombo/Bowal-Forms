1d239bd2970b5dceb3ee32132d49bb1309e1bab4
bowadao <samuelibrahim3029@gmail.com>
2024-07-23 20:09:33 +0100

    get forms

--- src/lib/get-forms.ts
+++ src/lib/get-forms.ts
@@ -4,28 +4,35 @@ import { getServerSession } from "./getServerSession";
 export const getForms = async () => {
   const session = await getServerSession();
 
-  if (!session) return;
+  try {
+    if (!session) return;
 
-  const forms = await prisma.form.findMany({
-    where: {
-      ownerId: session.user.id,
-    },
-    select: {
-      id: true,
-      title: true,
-      description: true,
-      shareableURL: true,
-      ownerId: true,
-      createdAt: true,
-      updatedAt: true,
-      lastOpened: true,
-      image: true,
-      theme: true,
-      questions: true,
-    },
-    orderBy: {
-      lastOpened: "desc",
-    },
-  });
-  return forms;
+    const forms = await prisma.form.findMany({
+      where: {
+        ownerId: session.user.id,
+      },
+      select: {
+        id: true,
+        title: true,
+        description: true,
+        shareableURL: true,
+        ownerId: true,
+        createdAt: true,
+        updatedAt: true,
+        lastOpened: true,
+        image: true,
+        theme: true,
+        questions: true,
+      },
+      orderBy: {
+        lastOpened: "desc",
+      },
+    });
+
+    return forms;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await prisma.$disconnect();
+  }
 };
--- src/views/sign-in-page.tsx
+++ src/views/sign-in-page.tsx
@@ -30,9 +30,7 @@ const SigninPage = ({ heading, callToAction }: AuthProps) => {
 
         <Button
           type="button"
-          onClick={() =>
-            signIn("google", { callbackUrl: "/forms", redirect: true })
-          }
+          onClick={() => signIn("google", { callbackUrl: "/forms" })}
           size="lg"
           className="w-full"
         >
