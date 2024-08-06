45cb6b69a861d99adaed42ff87fc7f9eeec5b6c5
bowadao <samuelibrahim3029@gmail.com>
2024-08-06 22:50:45 +0100

    server action for form response post

--- src/app/api/response/route.ts
+++ src/app/api/response/route.ts
@@ -3,19 +3,6 @@ import { prisma } from "@/lib/prisma-connect";
 
 export const GET = async (req: NextRequest) => {};
 
-export const POST = async (req: NextRequest) => {
-  const { formId, questionId, questionTitle, answer } = await req.json();
-
-  const response = await prisma.response.create({
-    data: {
-      formId,
-      questionId,
-      questionTitle,
-      answer,
-    },
-  });
-};
-
 export const DELETE = async (req: NextRequest) => {};
 
 export const PATCH = async (req: NextRequest) => {};
